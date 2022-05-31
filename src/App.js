46963b4a76beb1663dfd2110396e363688ad3b22
Rui Filipe Pinto Andrade <a24325@aemtg.pt>
2022-05-31 19:39:26 +0100

    kkk

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Hello, World!</h1>
+            Minha primeira aplicação em react.js!
         </p>
         <a
           className="App-link"
